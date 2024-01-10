5d7a1f11bffaa0d6e36c0868b5f20ffd660f3624
Ezekiel Lopez <ezekiel.lopez@mrjaffesclass.com>
2024-01-10 11:01:18 -0800

    Commit message

--- Die1/DieN.java
+++ Die1/DieN.java
@@ -1,22 +1,27 @@
+import java.util.Random;
+
 public class DieN {
+    private int sides;
+    private Random random;
 
-    private int value;  
-
-    public DieN() { 
-        roll();
+    public DieN(int sides) {
+        this.sides = sides;
+        this.random = new Random();
     }
 
-    public void roll() { 
-        value = (int)(Math.random()*6) + 1;
-    }    
-
-    public int getValue() { 
-        return value; 
+    public int roll() {
+        return random.nextInt(sides) + 1;
     }
 
-    public int rollAndGetValue() { 
-        roll(); 
-        return getValue();
-    }
+    public static void main(String[] args) {
+        // Example usage:
+        
+        // Create a 6-sided die
+        DieN sixSidedDie = new DieN(6);
 
-}   
+        // Roll the die
+        int result = sixSidedDie.roll();
+        System.out.println("The result of rolling a 6-sided die is: " + result);
+    }
+}
+
